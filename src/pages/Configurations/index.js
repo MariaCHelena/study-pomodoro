1d1099099f76525f36798c22a337ec06b97f241d
Maria <marianenacarvalho@gmail.com>
2024-01-02 12:35:03 -0300

    styling configuratins screen

--- src/pages/Configurations/index.js
+++ src/pages/Configurations/index.js
@@ -1,10 +1,46 @@
-import React from "react";
-import { Text } from "react-native";
+import React, { useState } from "react";
+import { Text, View, TextInput } from "react-native";
 
 import Telas from "../../components/Telas";
+import style from "./style";
 
-export default function Configurations(){
-    return <Telas>
-        <Text>Configurations</Text>
+export default function Configurations() {
+  const [focusTime, setFocusTime] = useState('00:00');
+  const [shortBreak, setShortBreak] = useState('00:00');
+  const [longBreak, setLongBreak] = useState('00:00');
+
+  return (
+    <Telas>
+      <View style={style.main}>
+        <Text style={style.title}>Configurations</Text>
+        <View style={style.container}>
+          <Text style={style.titleContainer}>Focus Time</Text>
+          <TextInput
+            value={focusTime}
+            onChangeText={(value) => setFocusTime(String(value))}
+            keyboardType='number-pad'
+            style={style.timerText}
+          />
+        </View>
+        <View style={style.container}>
+          <Text style={style.titleContainer}>Short Break</Text>
+          <TextInput
+            value={shortBreak}
+            onChangeText={(value) => setShortBreak(String(value))}
+            keyboardType='number-pad'
+            style={style.timerText}
+          />
+        </View>
+        <View style={style.container}>
+          <Text style={style.titleContainer}>Long Break</Text>
+          <TextInput
+            value={longBreak}
+            onChangeText={(value) => setLongBreak(String(value))}
+            keyboardType='number-pad'
+            style={style.timerText}
+          />
+        </View>
+      </View>
     </Telas>
+  );
 }
---
+++ src/pages/Configurations/style.js
@@ -0,0 +1,36 @@
+import { StyleSheet } from "react-native";
+import { cores } from "../../style";
+
+export default StyleSheet.create({
+    main: {
+        flex: 1,
+        alignItems: "flex-start",
+        justifyContent: "flex-start",
+        width: "100%"
+    },
+    title: {
+        fontSize: 24,
+        color: cores.white,
+        fontWeight: "bold",
+        textTransform: "uppercase",
+        marginVertical: 12,
+    },
+    container: {
+        width: "100%",
+        textAlign: "center",
+        justifyContent: "center",
+        paddingVertical: 20,
+        borderBottomWidth: 1,
+        borderBottomColor: cores.gray,
+        borderStyle: 'solid',
+    },
+    titleContainer: {
+        color: cores.white,
+        fontSize: 18
+    },
+    timerText: {
+        color: cores.white,
+        fontWeight: "bold",
+        fontSize: 24,
+    }
+})
